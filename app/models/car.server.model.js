64c060ffc498413cd4bcdfbf6ae4cadd658bbc42
Sarin Achawaranont <sarim.achawaranont@gmail.com>
2015-05-04 15:43:23 +0700

    fix car model bug

--- app/models/car.server.model.js
+++ app/models/car.server.model.js
@@ -16,52 +16,6 @@ var CarSchema = new Schema({
 		required: 'Please fill Car name',
 		trim: true
 	},
-	tech_id: {
-		type: String,
-		required: 'Please fill Technician ID',
-		trim: true
-	},
-	tech_fullname: {
-		type: String,
-		default: '',
-		required: 'Please fill Technician Name',
-		trim: true
-	},
-	tech_signdate: {
-		type: Date,
-		default: Date.now
-	},
-	tech_is_resign: {
-		type: Boolean,
-		default: false
-	},
-	tech_resigndate: {
-		type: Date,
-		default: Date.now
-	},
-	tech_holiday: {
-		type: Number,
-		default: 0,
-		required: 'Please fill Technician Holiday'
-	},
-	//Station Number: 0, 1, 2, 3, and so on..
-	tech_main_station: {
-		type: Number,
-		required: 'Please fill Technician Main Station'
-	},
-
-	tech_exholidays: [
-			{
-				start:Date,
-				end:Date
-			}
-	],
-	tech_skills: [
-			{
-				skill_id: String
-			}
-	],
-
 	created: {
 		type: Date,
 		default: Date.now
