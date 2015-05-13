d78be22d4735b010787ea7dafabe4e49cac4f3a6
Sarin Achawaranont <sarim.achawaranont@gmail.com>
2015-05-13 17:24:14 +0700

    improves car edit ui

--- public/modules/jobs/config/jobs.client.routes.js
+++ public/modules/jobs/config/jobs.client.routes.js
@@ -9,10 +9,10 @@ angular.module('jobs').config(['$stateProvider',
 			url: '/jobs',
 			templateUrl: 'modules/jobs/views/list-jobs.client.view.html'
 		}).
-		state('listJobsTel', {
-			url: '/jobs/tel',
-			templateUrl: 'modules/jobs/views/list-jobs-tel.client.view.html'
-		}).
+		// state('listJobsTel', {
+		// 	url: '/jobs/tel',
+		// 	templateUrl: 'modules/jobs/views/list-jobs-tel.client.view.html'
+		// }).
 		state('createJob', {
 			url: '/jobs/create',
 			templateUrl: 'modules/jobs/views/create-job.client.view.html'
@@ -21,10 +21,10 @@ angular.module('jobs').config(['$stateProvider',
 			url: '/jobs/:jobId',
 			templateUrl: 'modules/jobs/views/view-job.client.view.html'
 		}).
-		state('editJobTel', {
-			url: '/jobs/:jobId/tel',
-			templateUrl: 'modules/jobs/views/view-job-tel.client.view.html'
-		}).
+		// state('editJobTel', {
+		// 	url: '/jobs/:jobId/tel',
+		// 	templateUrl: 'modules/jobs/views/view-job-tel.client.view.html'
+		// }).
 		state('editJob', {
 			url: '/jobs/:jobId/edit',
 			templateUrl: 'modules/jobs/views/edit-job.client.view.html'
